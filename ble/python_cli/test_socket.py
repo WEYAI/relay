6068c71b46b48f803adc667ba99c8eb550f9e84c
weyai <840274774@qq.com>
2024-01-15 15:46:13 +0800

    one format

--- ble/python_cli/test_socket.py
+++ ble/python_cli/test_socket.py
@@ -7,22 +7,11 @@ import os
 import signal
 # server
 def handle_client(client_socket,request):
-
-
-    sniff_ble(client_socket,request)
-
-
-    # client_socket.close()
-
-def sniff_ble(client_socket,request):
     command = "python -u " + request["scriptCommand"]
     if "sniff" in command:
         command = "python -u " + request["scriptCommand"] + " -s COM18"
     elif "" in command: 
         pass
-    
-    
-
     print(command)
     # args = request.split()[1]
     process = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
@@ -32,49 +21,13 @@ def sniff_ble(client_socket,request):
         time.sleep(3)
         process.terminate()
     output, error = process.communicate()
+    print("进程输出。。。。。")
     print(output)
-    output = output.replace('\n', ',')
-    print(output)
-    client_socket.send(f"{output}\n".encode())
     print("send success....................")
-    if error:
-        client_socket.send(f"ERROR: {error}\n".encode())
-    client_socket.close()
-
-def ble_forge(client_socket,request):
-
-
-    # command ="python"+request["scriptCommand"]
-    command = "python advertiser.py"
-    # args = request.split()[1]
-    process = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                               universal_newlines=True)
-
-    output, error = process.communicate()
-
-    print(output)
     client_socket.send(f"{output}\n".encode())
     if error:
         client_socket.send(f"ERROR: {error}\n".encode())
     client_socket.close()
-
-def ble_relay(client_socket,request):
-    command = "python " + +request["scriptCommand"] + " -s COM14 "
-    # args = request.split()[1]
-    process = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                               universal_newlines=True)
-    if "sniff" in command:
-        time.sleep(3)
-        process.terminate()
-    output, error = process.communicate()
-    output = output.replace('\n', ',')
-    print(output)
-    print("test..........")
-    client_socket.send(f"{output}\n".encode())
-    if error:
-        client_socket.send(f"ERROR: {error}\n".encode())
-    client_socket.close()
-
 
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -95,17 +48,8 @@ def main():
         request = client_socket.recv(1024).decode()
         request = json.loads(request)
         print(request)
-        if "sniff" in str(request):
-            sniff_ble(client_socket, request)  # 处理客户端请求并关闭连接
-        elif "adver" in str(request):
-            print("adver........")
-            ble_forge(client_socket, request)
-
-
-        print("Connection closed")
-        # client_socket.close()  # 关闭客户端连接套接字并释放资源
-        # client_socket.close()
-         # 在实际应用中，你可能希望循环而不是在这里使用break，以便处理多个客户端连接。这里只是为了简化示例。
+        handle_client(client_socket, request)  # 处理客户端请求并关闭连接
+        print("Connection closed, waiting for client.........")
 
 
 if __name__ == "__main__":
